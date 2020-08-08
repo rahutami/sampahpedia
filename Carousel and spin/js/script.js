7155fe7726617affa502fe6ab7c0bc4db76d2f65
Denilsen Axel <55840688+DenilsenAxel@users.noreply.github.com>
2020-08-08 20:45:59 +0800

    Slide animation refined

--- Carousel and spin/js/script.js
+++ Carousel and spin/js/script.js
@@ -6,10 +6,27 @@ const dotsNav = document.querySelector('.information_nav');
 const dots = Array.from(dotsNav.children);
 const leftButton = document.querySelector('.information_button--left');
 const rightButton = document.querySelector('.information_button--right');
+const sectionTitle = document.querySelector('.section_title');
 
-const moveToSlide = (currentInfo, targetInfo) => {
+const slideWidth = slides[0].getBoundingClientRect().width;
+
+const setSlidePosition = (slide, index) => {
+    slide.style.left = slideWidth * index + 'px';
+};
+slides.forEach(setSlidePosition);
+
+const moveToSlide = (track, currentInfo, targetInfo) => {
+    track.style.transform = 'translateX(-' + targetInfo.style.left
     currentInfo.classList.remove('current_info');
     targetInfo.classList.add('current_info');
+
+    sectionTitle.classList.add('section_hidden');
+    dotsNav.classList.add('section_hidden');
+
+    setTimeout(function(){
+        sectionTitle.classList.remove('section_hidden');
+        dotsNav.classList.remove('section_hidden');
+    }, 500);
 };
 
 const moveDot = (currentDot, targetDot) => {
@@ -27,51 +44,49 @@ dotsNav.addEventListener('click', e => {
     const targetIndex = dots.findIndex(dot => dot === targetDot);
     const targetInfo = slides[targetIndex];
 
-    moveToSlide(currentInfo, targetInfo);
+    moveToSlide(track, currentInfo, targetInfo);
 
     moveDot(currentDot, targetDot);
 
 });
 
 leftButton.addEventListener('click', e => {
-    const currentInfo = track.querySelector('.current_info');
-    const currentDot = dotsNav.querySelector('.current_indicator');
+    const currentSlide = track.querySelector('.current_info');
+    const currentDot = dotsNav.querySelector('.current_indicator')
     const currentIndex = dots.findIndex(dot => dot === currentDot);
     if(currentIndex === 0) {
         const targetIndex = dots.length - 1;
-        const targetInfo = slides[targetIndex];
         const targetDot = dots[targetIndex];
-        moveToSlide(currentInfo, targetInfo);
+        const nextSlide = slides[targetIndex];
+        moveToSlide(track, currentSlide, nextSlide);
         moveDot(currentDot, targetDot);
     } else {
         const targetIndex = currentIndex - 1;
-        const targetInfo = slides[targetIndex];
         const targetDot = dots[targetIndex];
-        moveToSlide(currentInfo, targetInfo);
+        const nextSlide = slides[targetIndex];
+        moveToSlide(track, currentSlide, nextSlide);
         moveDot(currentDot, targetDot);
     }
 });
 
 rightButton.addEventListener('click', e => {
-    const currentInfo = track.querySelector('.current_info');
-    const currentDot = dotsNav.querySelector('.current_indicator');
+    const currentSlide = track.querySelector('.current_info');
+    const currentDot = dotsNav.querySelector('.current_indicator')
     const currentIndex = dots.findIndex(dot => dot === currentDot);
     if(currentIndex === dots.length - 1) {
         const targetIndex = 0;
-        const targetInfo = slides[targetIndex];
         const targetDot = dots[targetIndex];
-        moveToSlide(currentInfo, targetInfo);
+        const nextSlide = slides[targetIndex];
+        moveToSlide(track, currentSlide, nextSlide);
         moveDot(currentDot, targetDot);
     } else {
         const targetIndex = currentIndex + 1;
-        const targetInfo = slides[targetIndex];
         const targetDot = dots[targetIndex];
-        moveToSlide(currentInfo, targetInfo);
+        const nextSlide = slides[targetIndex];
+        moveToSlide(track, currentSlide, nextSlide);
         moveDot(currentDot, targetDot);
     }
 });
-
-// Lightslider
 
 $(document).ready(function() {
     $('.image-gallery').lightSlider({
